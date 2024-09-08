9ec02a3a4a6eaf3c44766ac38c03946df8cf24b7
Vinayak Jadhav <vinayak.jadhav333@gmail.com>
2024-09-09 00:56:10 +0100

    Updated pipeline and config

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ export async function run() {
             if (secretProperties.enabled) {
               const secret = await client.getSecret(secretProperties.name);
               const secretValue = secret.value;
-              exec(`echo "::add-mask::${secretValue}" && write-output "${secretProperties.name}=${secretValue}" | out-file -filepath ${process.env.GITHUB_ENV} -Encoding utf8 -append`,  {'shell':'pwsh'}, (error) => {
+              exec(`$secretvalue = "${secretValue}" && echo "::add-mask::${secretValue}" && write-output "${secretProperties.name}=${secretValue}" | out-file -filepath ${process.env.GITHUB_ENV} -Encoding utf8 -append`,  {'shell':'pwsh'}, (error) => {
                 if (error) {
                   console.error(`exec error: ${error}`);
                   return;
@@ -36,11 +36,11 @@ export async function run() {
             console.log(`Getting all secrets from ${keyvaultName} for pattern ${secretNamePattern}`);
             for await (const secretProperties of client.listPropertiesOfSecrets()) {
                 if (secretProperties.enabled) {
-                    if(secretProperties.name.match(secretNamePattern))
+                    if(secretProperties.name.includes(secretNamePattern))
                     {
                         const secret = await client.getSecret(secretProperties.name);
                         const secretValue = secret.value;
-                    exec(`echo "::add-mask::${secretValue}" && write-output "${secretProperties.name}=${secretValue}" | out-file -filepath ${process.env.GITHUB_ENV} -Encoding utf8 -append`,  {'shell':'pwsh'}, (error) => {
+                    exec(`$secretvalue = "${secretValue}" && echo "::add-mask::${secretValue}" && write-output "${secretProperties.name}=${secretValue}" | out-file -filepath ${process.env.GITHUB_ENV} -Encoding utf8 -append`,  {'shell':'pwsh'}, (error) => {
                         if (error) {
                         console.error(`exec error: ${error}`);
                         return;
@@ -60,7 +60,7 @@ export async function run() {
                 console.log(`Getting secret from ${keyvaultName} for name ${secretName}`);
                 const secret = await client.getSecret(secretName);
                 const secretValue = secret.value;
-                exec(`echo "::add-mask::${secretValue}" && write-output "${secretName}=${secretValue}" | out-file -filepath ${process.env.GITHUB_ENV} -Encoding utf8 -append`,  {'shell':'pwsh'}, (error) => {
+                exec(`$secretvalue = "${secretValue}" && echo "::add-mask::${secretValue}" && write-output "${secretName}=${secretValue}" | out-file -filepath ${process.env.GITHUB_ENV} -Encoding utf8 -append`,  {'shell':'pwsh'}, (error) => {
                     if (error) {
                         console.error(`exec error: ${error}`);
                         return;
